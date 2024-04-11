51483fba3620e4287af20c694c3157fbfca4630e
linda <nmandakh60@gmail.com>
2024-04-11 16:17:28 +0800

    Edit: member

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 import express from "express";
-import sales from "./data/sales.js";
-import { z } from "zod";
 import bcrypt from "bcryptjs";
-import adminRouter from "./routes/admin2.js";
 import memberRouter from "./routes/member.js"
-import abRouter from "./routes/address-book.js";
 import session from "express-session";
 import mysqlSession from "express-mysql-session";
 import db from "./utils/mysql2-connect.js";
-import dayjs from "dayjs";
 import cors from "cors";
 
 const MysqlStore = mysqlSession(session);
@@ -31,74 +26,31 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(
   session({
-    saveUninitialized: true,
-    resave: true,
+    saveUninitialized: true, // When the user hasn't used session yet, if still want to save the session
+    resave: true, // when the session content hasn't changed and still want to force save it
     secret: "kdjfsk94859348JHGJK85743",
     store: sessionStore,
-    //cookie: {
-    //  maxAge: 1200_000,
-    //}
   })
 );
 
-// 自訂的頂層的 middlewares
+// self made middlewares
 app.use((req, res, next) => {
-  res.locals.title = "小新的網站";
+  res.locals.title = "Some website";
   res.locals.pageName = "";
-  res.locals.session = req.session; // 讓 ejs 可以使用 session
+  res.locals.session = req.session; // let ejs be able to session
   res.locals.originalUrl = req.originalUrl;
   next();
 });
 
-/*
-
-// 測試用, 服務 react 發佈後的專案
-app.use("/", express.static("build"));
-app.get("*", (req, res) => {
-  res.send(`<!doctype html><html lang="zh"><head><meta charset="utf-8"/><link rel="icon" href="/favicon.ico"/><meta name="viewport" content="width=device-width,initial-scale=1"/><meta name="theme-color" content="#000000"/><meta name="description" content="Shinder react hooks"/><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"/><title>Shinder react hooks</title><script defer="defer" src="/static/js/main.6a205622.js"></script></head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div></body></html>`);
-});
-
-*/
-
-// 這個路由只接受 GET 方法, 路徑要為／
 app.get("/", (req, res) => {
-  res.locals.title = "首頁 - " + res.locals.title;
+  res.locals.title = "Home - " + res.locals.title;
   res.locals.pageName = "home";
-  // res.send("<h2>hello</2>"); // html 輸出
-  // res.send({a:1});  // json 輸出
-  // res.json({a:1, b:2});  // json 輸出
-  res.render("home", { name: "Shinder" });
-});
-/*
-app.get("/a.html", (req, res) => {
-  res.send("<h2>假的 a.html</2>");
-});
-*/
-app.get("/json-sales", (req, res) => {
-  res.locals.title = "json - " + res.locals.title;
-  res.locals.pageName = "json-sales";
-  res.render("json-sales", { sales });
+  res.render("home", { name: "Linda" });
 });
 
-app.get(/^\/m\/09\d{2}-?\d{3}-?\d{3}$/i, (req, res) => {
-  let u = req.url.split("?")[0]; // 排除 query string
-  u = u.slice(3); // 去掉前面的三個字元 /m/
-  u = u.split("-").join(""); // 去掉 -
-  res.json({ u });
-});
-
-app.use("/admins", adminRouter);
 app.use("/member", memberRouter);
 
-app.get("/try-sess", (req, res) => {
-  // req.session.myNum = req.session.myNum || 0;
-  req.session.myNum ||= 0; // 簡略的寫法
-  req.session.myNum++;
-  res.json(req.session);
-});
-
 app.get("/login", (req, res) => {
-  // 如果此用戶已經登入了, 跳轉到首頁
   if(req.session.admin){
     return res.redirect("/");
   }
@@ -114,19 +66,19 @@ app.post("/login", async (req, res) => {
     postData: req.body,
   };
   if(!account || !password){
-    output.error = "欄位資料不足";
+    output.error = "Information incomplete";
     output.code = 400;
     return res.json(output);
   }
   account = account.trim();
   password = password.trim();
 
-  const sql = "SELECT * FROM members WHERE email=?";
+  const sql = "SELECT * FROM mb_team_member WHERE user_name=?";
   const [rows] = await db.query(sql, [account]);
   
   if(!rows.length){
-    // 帳號是錯的
-    output.error = "帳號或密碼錯誤";
+    // Account is wrong
+    output.error = "Account or password is wrong";
     output.code = 420;
     return res.json(output);
   }
@@ -134,23 +86,19 @@ app.post("/login", async (req, res) => {
 
   const result = await bcrypt.compare(password, row.password);
   if(result){
-    // 帳號是對的, 密碼也是對的
     output.success = true;
     req.session.admin = {
       id: row.id,
-      email: account,
-      nickname: row.nickname
+      username: account,
     };
   } else {
-    // 密碼是錯的
-    output.error = "帳號或密碼錯誤";
+    output.error = "Account or password is wrong";
     output.code = 450;
   }
   res.json(output);
 });
+
 app.get("/logout", (req, res) => {
-  // if(req.session && req.session.admin)
-  // 簡略的寫法
   if(req.session?.admin){
     delete req.session.admin;
   }
@@ -162,70 +110,15 @@ app.get("/logout", (req, res) => {
 
 });
 
-app.get("/try-db", async (req, res) => {
-  const sql = "SELECT * FROM address_book LIMIT 3";
-  const [rows, fields] = await db.query(sql);
-  // fields: 資料表結構的相關訊息
-
-  res.json(rows);
-});
-
-app.get("/dayjs", (req, res) => {
-  const date1 = new Date();
-  const dayjs1 = dayjs();
-  res.json({
-    date1,
-    dayjs1: dayjs1.format("YYYY-MM-DD"),
-    dayjs2: dayjs1.format("YYYY-MM-DD HH:mm:ss"),
-  });
-});
-
-app.use("/address-book", abRouter);
-
-app.get("/zod", (req, res) => {
-  const v1 = "teest.com";
-  const v2 = "te@sest.com";
-  const schema = z.string().email({ message: "錯誤的電郵格式" });
-  const result1 = schema.safeParse(v1);
-  const result2 = schema.safeParse(v2);
-  res.send({ result1, result2 });
-});
-
-app.get("/zod2", (req, res) => {
-  const dataObj = {
-    name: "a",
-    email: "tttt",
-  };
-  const schema = z.object({
-    name: z.string().min(2, { message: "請填寫兩個字以上" }),
-    email: z.string().email({ message: "錯誤的電郵格式" }),
-  });
-  const result = schema.safeParse(dataObj);
-  res.send(result);
-});
-
-app.get("/yahoo", (req, res) => {
-  fetch("https://tw.yahoo.com/")
-  .then(r=>r.text())
-  .then(txt=>{
-    res.send(txt);
-  })
-  .catch(ex=>res.send("出錯啦"));
-});
-
-/* ************** 其他的路, 放在這行之前 *********** */
-// 靜態內容的資料夾
-
 app.use("/", express.static("public"));
 app.use("/bootstrap", express.static("node_modules/bootstrap/dist"));
 app.use("/jquery", express.static("node_modules/jquery/dist"));
 
-/* 404 頁面 */
 app.use((req, res) => {
   res.status(404).send(`<h2>404 走錯路了</h2>`);
 });
 
 const port = process.env.WEB_PORT || 3002;
 app.listen(port, () => {
-  console.log(`伺服器啟動 使用通訊埠 ${port}`);
+  console.log(`Server started at ${port}`);
 });
--- routes/member.js
+++ routes/member.js
@@ -5,6 +5,18 @@ import db from "../utils/mysql2-connect.js";
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+  const whiteList = ["/"];
+  let path = req.url.split("?")[0];
+  if (!whiteList.includes(path)) {
+    if (!req.session.admin) {
+      return res.redirect("/login");
+    }
+  }
+
+  next();
+});
+
 const getListData = async (req) => {
   let page = +req.query.page || 1;
 
@@ -19,27 +31,6 @@ const getListData = async (req) => {
         \`phone\` LIKE ${keywordEsc} 
         ) `;
   }
-  // const dateFormat = "YYYY-MM-DD";
-
-  // let date_begin = req.query.date_begin || "";
-  // if (dayjs(date_begin, dateFormat, true).isValid()) {
-  //   date_begin = dayjs(date_begin).format(dateFormat);
-  // } else {
-  //   date_begin = "";
-  // }
-  // if (date_begin) {
-  //   where += ` AND birthday >= '${date_begin}' `;
-  // }
-
-  // let date_end = req.query.date_end || "";
-  // if (dayjs(date_end, dateFormat, true).isValid()) {
-  //   date_end = dayjs(date_end).format(dateFormat);
-  // } else {
-  //   date_end = "";
-  // }
-  // if (date_end) {
-  //   where += ` AND birthday <= '${date_end}' `;
-  // }
 
   if (page < 1) {
     return { success: false, redirect: "?page=1" };
@@ -87,12 +78,98 @@ router.get("/", async (req, res) => {
   if (data.redirect) {
     return res.redirect(data.redirect);
   }
-  //   if (req.session.admin) {
-  //     res.render("address-book/list", data);
-  //   } else {
-  //     res.render("address-book/list-no-admin", data);
-  //   }
-  res.render("member/list", data);
+  if (req.session.admin) {
+    res.render("member/list", data);
+  } else {
+    res.render("member/list-no-admin", data);
+  }
+});
+
+router.get("/add", async (req, res) => {
+  res.locals.title = "Add new member - " + res.locals.title;
+  res.locals.pageName = "mb-add";
+  res.render("member/add");
+});
+
+router.post("/add", async (req, res) => {
+  const output = {
+    success: false,
+    bodyData: req.body,
+    errors: {},
+    error: "",
+  };
+
+  let isPass = true;
+
+  const schemaName = z.string().min(2, { message: "It's too short" });
+  const schemaEmail = z.string().email({ message: "Invalid email" });
+
+  const r1 = schemaName.safeParse(req.body.name);
+  if (!r1.success) {
+    isPass = false;
+    output.errors.name = r1.error.issues[0].message;
+  }
+  const r2 = schemaEmail.safeParse(req.body.email);
+  if (!r2.success) {
+    isPass = false;
+    output.errors.email = r2.error.issues[0].message;
+  }
+
+  let birthday = req.body.birthday;
+  if (dayjs(birthday, "YYYY-MM-DD", true).isValid()) {
+    birthday = dayjs(birthday).format("YYYY-MM-DD");
+  } else {
+    birthday = null;
+  }
+  req.body.birthday = birthday;
+
+  let result = {};
+  if (isPass) {
+    const checkEmailSql = "SELECT `id` FROM `mb_user` WHERE `email` = ?";
+
+    try {
+      const [rows] = await db.query(checkEmailSql, req.body.email);
+
+      if (rows.length) {
+        output.code = 1;
+        output.error = "This email exists. Try another";
+        res.json(output);
+        return;
+      }
+    } catch (ex) {
+      console.log(ex);
+      res.json(output);
+      return;
+    }
+
+    const checkPhoneSql = "SELECT `id` FROM `mb_user` WHERE `phone` = ?";
+
+    try {
+      const [rows] = await db.query(checkPhoneSql, req.body.phone);
+
+      if (rows.length) {
+        output["code"] = 2;
+        output["error"] = "This phone number has been registered already";
+        res.json(output);
+        return;
+      }
+    } catch (ex) {
+      console.log("Checking phone error" + ex);
+      output["error"] = "SQL error" + ex;
+      res.json(output);
+      return;
+    }
+
+    req.body.created_at = new Date();
+    const sql = "INSERT INTO `mb_user` SET ? ";
+
+    try {
+      [result] = await db.query(sql, [req.body]);
+      output.success = !!result.affectedRows;
+    } catch (ex) {}
+  }
+
+  res.json(output);
 });
 
 router.get("/edit/:id", async (req, res) => {
@@ -117,53 +194,98 @@ router.put("/edit/:id", async (req, res) => {
   const output = {
     success: false,
     bodyData: req.body,
+    errors: {},
     error: "",
   };
+  let isPass = true;
+
+  const schemaName = z.string().min(3, { message: "It's too short" });
+  const schemaEmail = z.string().email({ message: "Invalid email" });
+
   let id = +req.params.id || 0;
-  const { email, phone } = req.body;
+  const { email, phone, name } = req.body;
 
-  // TODO: 表單資料欄位檢查
-  const checkEmailSql = "SELECT `id` FROM `mb_user` WHERE `email` = ?";
+  const r1 = schemaName.safeParse(name);
+  if (!r1.success) {
+    isPass = false;
+    output.errors.name = r1.error.issues[0].message;
+  }
+  const r2 = schemaEmail.safeParse(email);
+  if (!r2.success) {
+    isPass = false;
+    output.errors.email = r2.error.issues[0].message;
+  }
 
-  try {
-    const [rows] = await db.query(checkEmailSql, email);
+  let birthday = req.body.birthday;
 
-    if (rows.length) {
-      output.code = 1;
-      output.error = "This email exists. Try another";
+  if (dayjs(birthday, "YYYY-MM-DD", true).isValid()) {
+    birthday = dayjs(birthday).format("YYYY-MM-DD");
+  } else {
+    birthday = null;
+  }
+
+  if (isPass) {
+    const checkEmailSql = "SELECT `id` FROM `mb_user` WHERE `email` = ?";
+
+    try {
+      const [rows] = await db.query(checkEmailSql, email);
+
+      if (rows.length) {
+        output.code = 1;
+        output.error = "This email exists. Try another";
+        res.json(output);
+        return;
+      }
+    } catch (ex) {
+      console.log(ex);
       res.json(output);
       return;
     }
-  } catch (ex) {
-    console.log(ex);
-    res.json(output);
-    return;
-  }
 
-  const checkPhoneSql = "SELECT `id` FROM `mb_user` WHERE `phone` = ?";
+    const checkPhoneSql = "SELECT `id` FROM `mb_user` WHERE `phone` = ?";
 
-  try {
-    const [rows] = await db.query(checkPhoneSql, phone);
+    try {
+      const [rows] = await db.query(checkPhoneSql, phone);
 
-    if (rows.length) {
-      output["code"] = 2;
-      output["error"] = "This phone number has been registered already";
+      if (rows.length) {
+        output["code"] = 2;
+        output["error"] = "This phone number has been registered already";
+        res.json(output);
+        return;
+      }
+    } catch (ex) {
+      console.log("Checking phone error" + ex);
+      output["error"] = "SQL error" + ex;
       res.json(output);
       return;
     }
-  } catch (ex) {
-    console.log("Checking phone error" + ex);
-    output["error"] = "SQL error" + ex;
-    res.json(output);
-    return;
+
+    const sql =
+      "UPDATE `mb_user` SET name=?, email=?, phone=?, birthday=? WHERE id=? ";
+    try {
+      const [result] = await db.query(sql, [name, email, phone, birthday, id]);
+      output.success = !!result.changedRows;
+    } catch (ex) {
+      output.error = ex.toString();
+    }
   }
 
-  const sql = "UPDATE `mb_user` SET ? WHERE id=? ";
-  try {
-    const [result] = await db.query(sql, [req.body, id]);
-    output.success = !!result.changedRows;
-  } catch (ex) {
-    output.error = ex.toString();
+  res.json(output);
+});
+
+router.delete("/:id", async (req, res) => {
+  let id = +req.params.id || 0;
+  const output = {
+    success: false,
+    id,
+  };
+
+  if (id >= 1) {
+    try {
+      const sql = `DELETE FROM mb_user WHERE id=${id}`;
+      const [result] = await db.query(sql);
+      output.success = !!result.affectedRows;
+    } catch (ex) {}
   }
 
   res.json(output);
